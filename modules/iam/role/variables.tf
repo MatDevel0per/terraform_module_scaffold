a4c172c1897ee9bc73743ba027f6bec0216147be
Mat <88615603+MatDevel0per@users.noreply.github.com>
2025-10-04 20:20:56 +0100

    feat: improved whitespace checks

--- modules/iam/role/variables.tf
+++ modules/iam/role/variables.tf
@@ -6,6 +6,10 @@ variable "principal_type" {
     condition     = length(trim(var.principal_type, " ")) > 0
     error_message = "The principal_type variable must not be an empty string."
   }
+  validation {
+    condition     = can(regex("^[^\\s]+$", var.principal_type))
+    error_message = "principal_type must not contain any whitespace (spaces, tabs, newlines, etc)."
+  }
 }
 variable "role_name" {
   description = "The name of the IAM role to be created."
@@ -14,5 +18,9 @@ variable "role_name" {
   validation {
     condition     = length(trim(var.role_name, " ")) > 0
     error_message = "The role_name variable must not be an empty string."
+  }
+  validation {
+    condition     = can(regex("^[^\\s]+$", var.role_name))
+    error_message = "role_name must not contain any whitespace (spaces, tabs, newlines, etc)."
   }
 }
