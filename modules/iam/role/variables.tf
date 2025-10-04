60b13bc79eda6a02d9902626b3c364b99aae0816
Mat <88615603+MatDevel0per@users.noreply.github.com>
2025-10-04 20:13:26 +0100

    feat: adding variable validation to prevent empty variables being passed

--- modules/iam/role/variables.tf
+++ modules/iam/role/variables.tf
@@ -2,9 +2,17 @@ variable "principal_type" {
   description = "The type of principal (e.g., AWS, Service, Federated) for the IAM role."
   type        = string
   nullable    = false
+  validation {
+    condition     = length(trim(var.principal_type, " ")) > 0
+    error_message = "The principal_type variable must not be an empty string."
+  }
 }
 variable "role_name" {
   description = "The name of the IAM role to be created."
   type        = string
   nullable    = false
+  validation {
+    condition     = length(trim(var.role_name, " ")) > 0
+    error_message = "The role_name variable must not be an empty string."
+  }
 }
