e4152cedac7208e0571dcfe8e1608f55a3d2a19e
Mat <88615603+MatDevel0per@users.noreply.github.com>
2026-09-23 20:13:23 +0100

    feat: moved to terraform data object
    Template files are yucky to write avoid where possible

--- modules/iam/role/main.tf
+++ modules/iam/role/main.tf
@@ -1,17 +1,47 @@
 locals {
   create_role = var.role_name != "" ? true : false
 }
+data "aws_iam_policy_document" "trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    dynamic "principals" {
+      for_each = var.principals
+
+      content {
+        type = principals.value.type
+        identifiers = [
+          for identifier in principals.value.identifiers :
+          principals.value.type == "AWS" && !startswith(identifier, "arn:") ?
+          "arn:aws:iam::${var.account_number}:role/${identifier}" :
+          identifier
+        ]
+      }
+    }
+    dynamic "condition" {
+      for_each = var.conditions
+
+      content {
+        test     = condition.value.operator
+        variable = condition.value.key
+        values   = [condition.value.value]
+      }
+    }
+  }
+}
 
 resource "aws_iam_role" "role" {
-  count = local.create_role ? 1 : 0
-  name  = var.role_name
-  path  = "/deployment/"
-  assume_role_policy = templatefile(
-    "${path.module}/policies/trust-relationship.json.tmpl",
-    {
-      principals    = var.principals
-      accountNumber = var.account_number
-      conditions    = var.conditions
-    }
-  )
+  count              = local.create_role ? 1 : 0
+  name               = var.role_name
+  path               = "/deployment/"
+  assume_role_policy = data.aws_iam_policy_document.trust.json
+  # assume_role_policy = templatefile(
+  #   "${path.module}/policies/trust-relationship.json.tmpl",
+  #   {
+  #     principals    = var.principals
+  #     accountNumber = var.account_number
+  #     conditions    = var.conditions
+  #   }
+  # )
 }
